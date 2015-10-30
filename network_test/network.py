567be6f6c422ab20501ec0536b343c51064eb5a0
yinruyi <916280067@qq.com>
2015-10-30 21:50:32 +0800

    test

--- network_test/network.py
+++ network_test/network.py
@@ -1,5 +1,7 @@
+#coding:utf-8
 import networkx as nx
 import matplotlib.pyplot as plt
+
 G = nx.Graph()
 G.add_edges_from([("1","2"),("1","3"),("1","4"),("1","5"),("4","5"),("4","6"),("5","6")])
 G.add_edge("7","8")
@@ -7,7 +9,7 @@ pos = nx.spring_layout(G)
  
 #colors = [1,2,2,2,3,1]
 #nx.draw_networkx_nodes(G,pos,node_color=colors)
-nx.draw_networkx_nodes(G,pos)
+nx.draw_networkx_nodes(G,pos,font_size=2000)
 nx.draw_networkx_edges(G,pos)
  
 plt.axis('off')
