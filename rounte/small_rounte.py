9a634ba218803683b894b9f8c28fbfd4e21e0121
R. Yin <916280067@qq.com>
2016-03-01 14:26:48 +0800

    TOP37景点得分结果

--- rounte/small_rounte.py
+++ rounte/small_rounte.py
@@ -32,3 +32,164 @@ class pretreatment():
         f = open(Path, "a+")
         line = f.write(string+"\n")
         f.close()
+
+class Rounte():
+    #景点之间走动的次数
+    def tran_data(self, mainAttraction, data):
+        result = []
+        for i in xrange(len(data)):
+            temp_result = []
+            temp = data[i].split()
+            if len(temp) > 0:
+                for j in xrange(len(temp)):
+                    new_temp = temp[j].split("/")
+                    if len(new_temp) == 2:
+                        if new_temp[1] == "tttttttt" and new_temp[0] in mainAttraction:
+                            temp_result.append(new_temp[0])
+            result.append(temp_result)
+        return result
+
+    def count(self, dataset):
+        resultSet = {}
+        for i in xrange(len(dataset)):
+            if dataset[i] in resultSet:
+                resultSet[dataset[i]] += 1
+            else:
+                resultSet[dataset[i]] = 1
+        return resultSet
+
+    def getRounte(self):
+        data = self.read_txt(abspath+"//data//tongjiblog998.txt")
+        mainAttraction = self.read_txt(abspath+"//data//mainAttraction.txt")
+        data = self.tran_data(mainAttraction, data)
+        rounteList = []
+        for i in xrange(len(data)):
+            temp = data[i]
+            if len(temp) > 1:
+                for j in xrange(len(temp)-1):
+                    if temp[j] != temp[j+1]:
+                        rounteTemp = (temp[j],temp[j+1])
+                        rounteList.append(rounteTemp)
+        rounteSet = self.count(rounteList)
+        return rounteSet
+
+class hits():
+    def getHits(self):
+        resultSet = {}
+        hits_data = self.read_txt(abspath+"//data//hits.txt")
+        mainAttraction = self.read_txt(abspath+"//data//mainAttraction.txt")
+        hitsSet = self.transData(hits_data)
+        for k,v in hitsSet.items():
+            if k in mainAttraction:
+                resultSet[k] = float(v)
+        #print resultSet,len(resultSet)
+        resultSet = self.set0_1Result(resultSet)
+        return resultSet
+
+
+    def transData(self, dataset):
+        Set = {}
+        for i in xrange(len(dataset)):
+            temp = dataset[i]
+            temp = temp.split(u",")
+            Set[temp[0]] = temp[1]
+        return Set
+
+    def set0_1Result(self, dataset):
+        temp = []
+        for k,v in dataset.items():
+            temp.append(v)
+        max_num = max(temp)
+        for k,v in dataset.items():
+            dataset[k] = v*1.0/max_num
+        return dataset
+
+class sentiment():
+    def getSentiment(self):
+        sentimentResult = {}
+        mainAttraction = self.read_txt(abspath+"//data//mainAttraction.txt")
+        sentiment_data = self.read_txt(abspath+"//data//sentiment.txt")
+        sentimentSet = self.transData(sentiment_data)
+        for k,v in sentimentSet.items():
+            if k in mainAttraction:
+                sentimentResult[k] = float(v)
+        sentimentResult = self.set0_1Result(sentimentResult)
+        return sentimentResult
+
+class feature():
+    def getFeature(self):
+        attraction_featureSet = {}
+        mainAttraction = self.read_txt(abspath+"//data//mainAttraction.txt")
+        for i in xrange(len(mainAttraction)):
+            path = abspath+"//data//result_data//"+str(i)+".txt"
+            txt = self.read_txt(path)
+            txtFeature = self.txt2feature(txt)
+            #print txtFeature
+            if (txtFeature[0] == "" or txtFeature[0] == " ") and len(txtFeature) == 1:
+            #if len(txtFeature) == 1:
+                attraction_featureSet[mainAttraction[i]] = []
+            else:
+                attraction_featureSet[mainAttraction[i]] = txtFeature
+        attraction_featureSet = self.fixFeatureSetResult(attraction_featureSet)
+        attraction_featureSet = self.set0_1Result(attraction_featureSet)
+        return attraction_featureSet
+
+    def fixFeatureSetResult(self, dataset):
+        result = {}
+        for k,v in dataset.items():
+            result[k] = len(list(set(v)))
+        return result
+
+    def txt2feature(self, dataset):
+        feature = []
+        for i in xrange(len(dataset)):
+            temp = dataset[i].split(",")
+            feature.append(temp[0])
+        return feature
+
+class DataAnalysis(pretreatment, Rounte, hits, sentiment, feature):
+    def makePlus(self, rounteSet,  section, method = "AX"):
+        #计算AX之和或者XB之和
+        result = []
+        if method == "AX":
+            for k,v in rounteSet.items():
+                if k[0] == section:
+                    result.append(v)
+        elif method == "XB":
+            for k,v in rounteSet.items():
+                if k[1] == section:
+                    result.append(v)
+        #print result
+        result = sum(result)
+        return result
+
+    def main(self):
+        rounteResult = {}
+        print "test"
+        rounteSet = self.getRounte()
+        #print rounteSet,len(rounteSet)
+        rounteSet01 = self.set0_1Result(rounteSet)
+        rounteSet = self.getRounte()#路径次数
+        hitsSet = self.getHits()#hits分数
+        #print hitsSet
+        sentimentSet = self.getSentiment()#sentiment分数
+        #print sentimentSet
+        featureSet = self.getFeature()
+        print featureSet
+        for k,v in rounteSet.items():
+            AXplus = self.makePlus(rounteSet,k[0],"AX")
+            #print AXplus
+            XBplus = self.makePlus(rounteSet,k[1],"XB")
+            A_part = v*hitsSet[k[0]]*sentimentSet[k[0]]*featureSet[k[0]]/AXplus
+            B_part = v*hitsSet[k[1]]*sentimentSet[k[0]]*featureSet[k[0]]/XBplus
+            point = rounteSet01[k]*(A_part+B_part)
+            rounteResult[k] = point
+        print rounteResult#得到结果
+        writeResutlt = []
+        for k,v in rounteResult.items():
+            temp = [k[0],k[1],v]
+            writeResutlt.append(temp)
+        self.writeMatrix(writeResutlt,"result.txt")
+
+if __name__=='__main__':
+    DataAnalysis().main()       
